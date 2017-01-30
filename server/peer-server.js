4333b33636feb38dafd73ca64af33d573b3a2697
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-01-30 17:10:57 +0100

    port 9000 for peerjs server

--- server/peer-server.js
+++ server/peer-server.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var peer = require('peer')
 var PeerServer = peer.PeerServer({
-  port: 3000,
+  port: 9000,
   ssl: {
     key: fs.readFileSync('/etc/letsencrypt/live/game.zaph.pw/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/game.zaph.pw/fullchain.pem')
