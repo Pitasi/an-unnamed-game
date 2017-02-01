9df49b4d905685d2ab07dd79b0acc479055b8647
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-02-01 16:48:35 +0100

    10 eatable black balls

--- client/scripts/ball.js
+++ client/scripts/ball.js
@@ -4,10 +4,14 @@ require('../styles/ball.less')
 
 function Ball (props) {
   this.active = true
-  this.size = 30
-  this.bounceSpeed = 0.96
+  this.mass = 30
+  this.friction = 0.96
+  this.padding = 12
   this.v = { x: 0, y: 0 }
-  this.pos = { x: (window.innerWidth-this.size)/2, y: (window.innerHeight-this.size)/2 }
+  this.pos = {
+    x: Math.floor(Math.random()*((window.innerWidth-this.mass)+1)),
+    y: Math.floor(Math.random()*((window.innerHeight-this.mass)+1)),
+  }
   this.conn = props.conn
   this.color = randomColor()
   this.conn.send({color: this.color})
@@ -25,37 +29,57 @@ function Ball (props) {
     this.active = false
   }
   setInterval(() => {
-    if (new Date() - this.lastUpdate > 1000) deactive();
+    if (new Date() - this.lastUpdate > 2500) deactive();
   }, 500)
   this.conn.on('close', () => { deactive() })
   this.conn.on('error', () => { deactive() })
 
   // methods
   this.updatePosition = () => {
-    this.v.x = this.v.x * this.bounceSpeed
-    this.v.y = this.v.y * this.bounceSpeed
+    this.v.x = this.v.x * this.friction
+    this.v.y = this.v.y * this.friction
 
     let newpos = {
-      y: parseInt(this.pos.y + this.v.y / 50),
-      x: parseInt(this.pos.x + this.v.x / 50)
+      y: parseInt(this.pos.y + this.v.y / this.mass),
+      x: parseInt(this.pos.x + this.v.x / this.mass)
     }
 
-    if (newpos.x<0) { newpos.x = 0; this.v.x = -this.v.x; }
-    if (newpos.y<0) { newpos.y = 0; this.v.y = -this.v.y; }
-    if (newpos.x>document.documentElement.clientWidth-this.size) { newpos.x = document.documentElement.clientWidth-this.size; this.v.x = -this.v.x; }
-    if (newpos.y>document.documentElement.clientHeight-this.size) { newpos.y = document.documentElement.clientHeight-this.size; this.v.y = -this.v.y; }
+    if (newpos.x<0) { newpos.x = 0; this.v.x = -this.v.x }
+    if (newpos.y<0) { newpos.y = 0; this.v.y = -this.v.y }
+    if (newpos.x>document.documentElement.clientWidth-this.mass-this.padding) {
+      newpos.x = document.documentElement.clientWidth-this.mass-this.padding
+      this.v.x = -this.v.x
+    }
+    if (newpos.y>document.documentElement.clientHeight-this.mass-this.padding) {
+      newpos.y = document.documentElement.clientHeight-this.mass-this.padding
+      this.v.y = -this.v.y
+    }
 
     this.pos = newpos
+  }
+
+  this.invertDirection = () => {
+    this.v.x = - this.v.x
+    this.v.y = - this.v.y
+  }
+
+  this.getRadius = () => { return (this.mass + this.padding)/2 } // TODO: 12 is padding + border, find a way to remove that
+  this.getCenter = () => {
+    return {
+      x: this.pos.x + (this.mass+this.padding)/2,
+      y: this.pos.y + (this.mass+this.padding)/2
+    }
   }
 }
 
 function BallComponent (props) {
   return (<div style={{
     background: props.ball.color,
-    height: props.ball.size,
-    width: props.ball.size,
+    height: props.ball.mass,
+    width: props.ball.mass,
     left: props.ball.pos.x + 'px',
-    top: props.ball.pos.y + 'px'
+    top: props.ball.pos.y + 'px',
+    zIndex: props.ball.mass
   }}
   className="ball"></div>)
 }
--- client/scripts/main.js
+++ client/scripts/main.js
@@ -29,20 +29,60 @@ class MainContainer extends React.Component {
       })
     })
     this.peer.on('error', (err) => { alert(err) })
+
+    for (var i = 0; i < 10; i++) {
+      let debugBall = new Ball({conn: {peer: 'debug'+i, send:() => {}, on: () => {}}})
+      debugBall.color = 'black'
+      debugBall.mass = 10
+      this.balls.push(debugBall)
+    }
   }
 
   createBall(conn) {
     this.balls.push(new Ball({conn: conn}))
   }
 
-  deleteBalls() {
+  deleteInactiveBalls() {
     this.balls = this.balls.filter((b) => {return b.active})
   }
 
+  checkCollision(b1, b2) {
+    if (b1.mass == b2.mass) return
+    if (b2.mass > b1.mass) {
+      let tmp = b1
+      b1 = b2
+      b2 = tmp
+    }
+    // b1 is the biggest ball now
+
+    let c1 = b1.getCenter(),
+        c2 = b2.getCenter()
+    let r1 = b1.getRadius(),
+        r2 = b2.getRadius()
+
+    let a = r1 > r2 ? r1 : r2,
+        x = c1.x - c2.x,
+        y = c1.y - c2.y;
+    let d = Math.sqrt(x*x + y*y)
+    if (d < r1) {
+      // b1 eats b2
+      b1.mass = b1.mass + b2.mass
+      b2.active = false
+    }
+  }
+
   gameLoop() {
-    this.deleteBalls()
+    this.deleteInactiveBalls()
+
+    for (var i in this.balls)
+      this.balls[i].updatePosition()
+
+    for (var i = 0; i < this.balls.length-1; i++)
+      for (var j = i + 1; j < this.balls.length; j++)
+        this.checkCollision(this.balls[i], this.balls[j])
+
+
     let ballComponents = this.balls.map((b) => {
-      b.updatePosition()
       return <BallComponent ball={b} key={b.conn.peer} />
     })
     this.setState({ballComponents: ballComponents})
